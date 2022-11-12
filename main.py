946298fa5025e28a96df9fb8d6a5a34efdfe017e
viniciusoliveirabittencourt <viniciusbittencourt2003@gmail.com>
2022-11-11 22:26:10 -0300

    add input to function and test not passing

--- main.py
+++ main.py
@@ -2,7 +2,15 @@ def main():
     user_option = None
 
     while user_option != 0:
-        print("working!")
-        break
-
-    print("Goodbye :)")
+        try:
+            input(
+                "Please, select one of the options below:\n"
+                "1 - AI guess the number\n"
+                "2 - Player guess the number\n"
+                "3 - Player X AI\n"
+                "4 - Player X Player\n"
+                "0 - Leave\n"
+            )
+            break
+        except ValueError:
+            raise ValueError
--- test/test_main.py
+++ test/test_main.py
@@ -1,7 +1,16 @@
 from main import main
 
 
-def test_menu(capsys):
+def test_menu_print_options(capsys):
     main()
     captured = capsys.readouterr()
-    assert captured.out == "working!\nGoodbye :)\n"
+    expected = (
+        "Please, select one of the options below:"
+        "1 - AI guess the number"
+        "2 - Player guess the number"
+        "3 - Player X AI"
+        "4 - Player X Player"
+        "0 - Leave"
+    )
+
+    assert captured.out == expected
