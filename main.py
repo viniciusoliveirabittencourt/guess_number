a86af8ba5424793f02e2b15afae59079c238b67f
viniciusoliveirabittencourt <viniciusbittencourt2003@gmail.com>
2022-11-13 20:53:41 -0300

    print menu anda test print menu working

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 def main():
-    user_option = None
+    print(
+        "Please, select one of the options below:\n"
+        "1 - AI guess the number\n"
+        "2 - Player guess the number\n"
+        "3 - Player X AI\n"
+        "4 - Player X Player\n"
+        "0 - Leave\n"
+        "Input value: "
+    )
 
-    while user_option != 0:
-        try:
-            input(
-                "Please, select one of the options below:\n"
-                "1 - AI guess the number\n"
-                "2 - Player guess the number\n"
-                "3 - Player X AI\n"
-                "4 - Player X Player\n"
-                "0 - Leave\n"
-            )
-            break
-        except ValueError:
-            raise ValueError
+
+main()
--- test/test_main.py
+++ test/test_main.py
@@ -2,15 +2,17 @@ from main import main
 
 
 def test_menu_print_options(capsys):
-    main()
-    captured = capsys.readouterr()
-    expected = (
-        "Please, select one of the options below:"
-        "1 - AI guess the number"
-        "2 - Player guess the number"
-        "3 - Player X AI"
-        "4 - Player X Player"
-        "0 - Leave"
-    )
 
-    assert captured.out == expected
+    main()
+
+    caps = capsys.readouterr()
+
+    assert (
+        "Please, select one of the options below:\n"
+        "1 - AI guess the number\n"
+        "2 - Player guess the number\n"
+        "3 - Player X AI\n"
+        "4 - Player X Player\n"
+        "0 - Leave\n"
+        "Input value: \n"
+    ) == caps.out
